d2b1f2b22bf2a19f59dbdd4b0704a0e31a82dd42
Alexina Chong <alexina.chong@gmail.com>
2017-12-15 14:22:00 -0800

    Add various Int sets

--- my_hash_map/lib/p01_int_set.rb
+++ my_hash_map/lib/p01_int_set.rb
@@ -1,19 +1,37 @@
 class MaxIntSet
   def initialize(max)
+    @max = max
+    @store = Array.new(max+1) { false }
   end
 
   def insert(num)
+    if is_valid?(num)
+      @store[num] = true
+    else
+      raise "Out of bounds"
+    end
   end
 
   def remove(num)
+    if is_valid?(num)
+      @store[num] = false
+    else
+      raise "Out of bounds"
+    end
   end
 
   def include?(num)
+    if is_valid?(num)
+      @store[num]
+    else
+      raise "Out of bounds"
+    end
   end
 
   private
 
   def is_valid?(num)
+    -1 < num && num <= @max
   end
 
   def validate!(num)
@@ -27,18 +45,21 @@ class IntSet
   end
 
   def insert(num)
+    @store[num % num_buckets] << num
   end
 
   def remove(num)
+    @store[num % num_buckets].delete(num)
   end
 
   def include?(num)
+    @store[num % num_buckets].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -55,18 +76,30 @@ class ResizingIntSet
   end
 
   def insert(num)
+    unless include?(num)
+      @store[num % num_buckets] << num
+      @count += 1
+      if @count > num_buckets
+        resize!
+      end
+    end
   end
 
   def remove(num)
+    if include?(num)
+      @store[num % num_buckets].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    @store[num % num_buckets].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -74,5 +107,10 @@ class ResizingIntSet
   end
 
   def resize!
+    elements = @store.flatten
+    @store = Array.new(2 * num_buckets) { Array.new }
+    @count = 0
+    elements.each { |el| insert(el) }
+
   end
 end
