84d0c08ec3c224af06b7266804a7b0fd0b22c384
Alexina Chong <alexina.chong@gmail.com>
2017-12-15 15:11:37 -0800

    Add hash functions

--- my_hash_map/lib/p02_hashing.rb
+++ my_hash_map/lib/p02_hashing.rb
@@ -4,11 +4,17 @@ end
 
 class Array
   def hash
+    hashes = self.map { |el| el.hash }
+    hash = 0
+    hashes.each_with_index { |el, i| hash += el * i }
+    hash % 10**9
   end
 end
 
 class String
   def hash
+    hashes_array = chars.map { |el| el.ord.hash }
+    hashes_array.hash
   end
 end
 
@@ -16,6 +22,11 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    keys = self.keys.sort
+    hashes_array = []
+    keys.each do |k|
+      hashes_array.push([k, self[k]].hash)
+    end
+    hashes_array.hash
   end
 end
